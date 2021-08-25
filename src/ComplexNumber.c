9a56acc3a596699994f01b13d4ba0d3ce15222d5
Eder Garcia Lastiri <ic19egl@antares.dci.uia.mx>
2021-08-25 17:15:49 -0500

    Adding libs and sources

---
+++ src/ComplexNumber.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../libs/ComplexNumber.h"
+
+static ComplexNumber initNewComplexNumber(ComplexNumber newComplexNumber);
+
+static ComplexNumber initNewComplexNumber(ComplexNumber newComplexNumber){
+  newComplexNumber->real=0;
+  newComplexNumber->imaginary=0;
+  return newComplexNumber;
+}
+
+ComplexNumber newComplexNumber (void){
+  ComplexNumber myComplexNumber=NULL;
+  myComplexNumber=malloc(sizeof(struct _Complex_Number));
+  myComplexNumber=initNewComplexNumber(myComplexNumber);
+  return myComplexNumber;
+}
+
+
+ComplexNumber setComplexNumber(ComplexNumber phasor, float real, float imaginary){
+
+}
+
+ComplexNumber getComplexNumberComponents(ComplexNumber phasor, float *real, float *imaginary){
+
+}
+
+ComplexNumber freeComplexNumber(ComplexNumber phasor){
+  
+}
